ba53c7833b9332fb9d1045cb053351cb54482b38
Aryan Sharma <76253876+Aryan110303@users.noreply.github.com>
2023-12-22 22:17:57 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge"> 
+    <title>JP Academy</title>
+    <meta name="description" content="The JP Educational Trust Society also gave shape to professional colleges so that the youth can seek self satisfying jobs. Thus came up JP Institute of Hotel Management and Catering Technology, JP Institute of Education, JP Institute of Applied Sciences and JP Institute of Engineering and Technology." />
+    <meta name="keywords" content="schools meerut, schools uttar pradesh, schools uttarakhand, schools bihar, schools rajasthan, schools NCR, best   schools meerut, best schools uttar pradesh, best schools uttarakhand, best schools bihar, best schools rajasthan, best   schools NCR, top 10 schools meerut, top 10 schools uttar pradesh, top 10 schools uttarakhand, top 10 schools bihar, top 10   schools rajasthan, top 10 schools NCR" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/medical-icon.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link href="css/owl.carousel.min.css" rel="stylesheet">   
+    <link rel="stylesheet" href="css/camera.css">
+    <link href="css/bootstrap-4-hover-navbar.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet">
+</head>
+<body>
+  <header id="sticky-header">
+  <div class="toparea">
+    <div class="container" style="padding: 0 !important; margin: 0 !important; width: 100% !important; max-width: 100% !important;">
+      <div class="row align-items-center">
+
+        <div class="col-lg-10 col-md-12 col-12">
+          <div class="row">
+            <div class="col-lg-2 col-md-12 col-12" style="padding:0% !important">
+              <ul class="top-social fleft">
+                <li><a href="https://www.facebook.com/jpacademymeerut.in" target="_blank"><i style="color: blue;" class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/jpacademymeerut" target="_blank"><i style="color: orange;" class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.youtube.com/@jpgroupmeerut5994" target="_blank"><i style="color: red;" class="fa fa-youtube"></i></a></li>
+              </ul>
+            </div>
+            <div class="col-lg-10 col-md-12 col-12">
+              <ul class="toplist fleft">
+                 <li><a href="mailto:jp_academy_meerut@yahoo.co.in"><i class="fa fa-envelope-o"></i>jp_academy_meerut@yahoo.co.in</a></li>
+                 <!-- <li><a href="mailto:shiv.rohitsharma@gmail.com"><i class="fa fa-envelope-o"></i>shiv.rohitsharma@gmail.com</a></li> -->
+                 <li><a href=""><i class="fa fa fa-phone"></i>+91-9068999713</a></li> 
+                 <li><a href=""><i class="fa fa fa-phone"></i>+91-9068999728 </a></li>
+                 <li><a href="">Affiliation Number: 2130247</a></li>
+                 <!-- <li> <a href="http://jpacademymeerut.com/admin/site/teacherlogin" target="_blank">Login Teacher</a></li> 
+                 <li> <a href="http://jpacademymeerut.com/admin/site/userlogin" target="_blank">Login Student</a></li>             -->
+              </ul>
+           </div>
+          </div><!--./row-->  
+        </div><!--./col-lg-8-->
+        <div class="col-lg-2 col-md-12 col-12" >
+            <a style="margin: 0 20px;" href="admission.php" class="admission-btn light_text">Admission 2024-25</a>
+          
+        </div>    
+      </div><!--./row-->
+     </div><!--./container--> 
+    </div><!--./toparea-->
+  <div class="headerbottom">  
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark menubg btco-hover-menu">
+        <a class="navbar-brand logo" href="index.php"><img src="images/logo.png" /></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"><i class="fa fa-bars navfa"></i></span>
+            </button>
+
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto nav-menu navborder navigation">
+            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link dropdown-toggle" href="about.php">About</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item arrowmenu" href="chairman.php">Message of Chairman</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="president.php">Message of President</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="director.php">Message of Director </a></li>
+                  <li><a class="dropdown-item arrowmenu" href="principal.php">Message of Principal  </a></li>
+                 
+                </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdowninfraLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Activities
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdowninfraLink">
+                <li><a class="dropdown-item arrowmenu" href="co-curricular-act.php">Co-curricular</a></li>
+                <li><a class="dropdown-item arrowmenu" href="sports.php">Sports</a></li>
+                <li><a class="dropdown-item arrowmenu" href="events.php">Events</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdowntrainingLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Facilities
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdowntrainingLink">
+                  <li><a class="dropdown-item arrowmenu" href="mess-medical-care.php">Mess & Medical Care</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="residential-block.php">Residential Blocks</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="computertraning.php">Computer Training</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="library.php">Library</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="transport.php">Transport</a></li>
+                </ul>
+              </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Academics
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item arrowmenu" href="our-approach.php">Our Approach</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="academic-block.php">Academic Calender</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="laboratories.php">Labs</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="educational-features.php">Educational Features</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="remedial-classes.php">Remedial Classes</a></li>
+                  <li><a class="dropdown-item arrowmenu" href="rules-and-regulations.php">Rules and Regulations</a></li>
+                </ul>
+              </li>
+
+              
+            <li class="nav-item"><a class="nav-link" href="affiliation.php">Affiliation</a></li>  
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+          </ul>
+        </div>
+     </nav>
+    </div> 
+   </div><!--./headerbottom-->   
+  </header>
